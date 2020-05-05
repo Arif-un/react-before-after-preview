1fadefe5096a239f0a149acb91143a1455d61266
Arif Uddin <arifunctg@gmail.com>
2020-05-05 23:53:08 +0600

    init

---
+++ src/ReactBeforeAfter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+function ReactBeforeAfter({ height, width }) {
+  return (
+    <div className="_react-b-a-wrp" style={{ height: height, width: width }}>
+      <img src="https://images.unsplash.com/photo-1558980394-a3099ed53abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
+    </div>
+  )
+}
+
+export default ReactBeforeAfter
