1f9a560c38b2082c9cc9ee343ee046edca867edd
Arif Uddin <arifunctg@gmail.com>
2020-05-06 23:54:56 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import ReactBeforeAfter from './ReactBeforeAfter'
 function App() {
   return (
     <div>
-      <ReactBeforeAfter height={300} width={500} />
+      <ReactBeforeAfter
+        height={300}
+        width={500}
+        //before={<img src="https://images.unsplash.com/photo-1558980394-a3099ed53abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="" />}
+        //after={<img src="https://images.unsplash.com/photo-1558980394-a3099ed53abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="" />}
+      before={<h1>asdasasdfasdfasfdda</h1>}
+      after={<h1>asdasasdfasdfasfdda</h1>}
+      />
     </div>
   );
 }
