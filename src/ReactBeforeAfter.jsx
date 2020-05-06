d93263f6cb27d1e375b3c983c96de339b48ce944
Arif Uddin <arifunctg@gmail.com>
2020-05-06 23:54:43 +0600

    action added

--- src/ReactBeforeAfter.jsx
+++ src/ReactBeforeAfter.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './App.css'
-function ReactBeforeAfter({ height, width }) {
+function ReactBeforeAfter({ height, width, before, after }) {
+  /*  var m = document.getElementById('move');
+   m.addEventListener('mousedown', mouseDown, false);
+   window.addEventListener('mouseup', mouseUp, false); */
+  console.log('render')
+
+  const bar = useRef(null)
+  const con = useRef(null)
+  let cx = 0, tx = 0
+  const [Xpos, setXpos] = useState(null)
+
+  function removeEvent() {
+    console.log('removed')
+    document.removeEventListener('mousemove', move);
+    document.onmousemove = null
+  }
+
+  function mouseDown(e) {
+    cx = e.target.getBoundingClientRect().x
+    console.log(e.clientX, e.target.getBoundingClientRect().x)
+    setXpos(e.target.getBoundingClientRect().x)
+    document.addEventListener('mousemove', move);
+  }
+
+  function mouseOnContainer() {
+    /* bar.current.onClick = () => {
+      console.log('clicked')
+    } */
+  }
+
+  function mouseOutContainer() {
+    // console.log("out")
+    // window.removeEventListener('mousemove', move, true);
+  }
+
+  function move(e) {
+    //  e.preventDefault();
+    let x = e.clientX - cx
+    console.log(e.clientX, e.target.getBoundingClientRect().x)
+    bar.current.style.left = x + 'px';
+  };
   return (
-    <div className="_react-b-a-wrp" style={{ height: height, width: width }}>
-      <img src="https://images.unsplash.com/photo-1558980394-a3099ed53abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
+    <div
+      onMouseOver={mouseOnContainer}
+      onMouseOut={mouseOutContainer}
+      className="_react-b-a-wrp"
+      style={{ height: height, width: width }}
+    >
+      <div style={{ height: height }} className="_before_after _before">{before}</div>
+      <div
+        ref={bar}
+        style={{ height: height }}
+        onMouseDown={mouseDown}
+        onMouseUp={removeEvent}
+        onMouseOut={removeEvent}
+        className="_bar"></div>
+      <div style={{ height: height }} className="_before_after">{after}</div>
     </div>
   )
 }
